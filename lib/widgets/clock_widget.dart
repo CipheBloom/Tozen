7a0f15bdc4bc5d5809ebb54c5c51200613c47c0b
yougraj <yougrajbora.developer@gmail.com>
2025-01-03 17:29:41 +0530

    adding a 1.0.0 version of the app

---
+++ lib/widgets/clock_widget.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class ClockWidget extends StatefulWidget {
+  @override
+  _ClockWidgetState createState() => _ClockWidgetState();
+}
+
+class _ClockWidgetState extends State<ClockWidget> {
+  late Timer _timer;
+  DateTime _currentTime = DateTime.now();
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        _currentTime = DateTime.now();
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          'Current Time',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          '${_currentTime.hour.toString().padLeft(2, '0')}:${_currentTime.minute.toString().padLeft(2, '0')}:${_currentTime.second.toString().padLeft(2, '0')}',
+          style: TextStyle(fontSize: 48, fontWeight: FontWeight.w300),
+        ),
+      ],
+    );
+  }
+}
